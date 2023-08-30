bf1c8cc50b275c240bf58364d9d701b5c973f606
Fanantenana Emmanuel <emmanuel3609@gmail.com>
2023-08-30 17:21:27 +0300

    date validation level 2

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,6 +11,8 @@ const yearLabel = document.querySelector("#yearLabel")
 const dayInvalidText = document.querySelector("span.invalid#day")
 const monthInvalidText = document.querySelector("span.invalid#month")
 const yearInvalidText = document.querySelector("span.invalid#year")
+const invalidDateText = document.querySelector(".invalidDate")
+const futureDateText = document.querySelector(".futureDate")
 
 let dayValue = ""
 let monthValue = ""
@@ -28,24 +30,31 @@ yearInput.addEventListener("change", () => {
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
+    let validDate = 0
     if (!dayValue) {
         dayLabel.classList.add("error")
         dayInput.classList.add("error")
         dayRequiredText.classList.add("show")
+        invalidDateText.classList.remove("show")
+        futureDateText.classList.remove("show")
     } else {
         if (dayValue > 0 && dayValue <= 31) {
-        // if (0 < dayValue <= 31) {
             console.log('valid day')
             dayLabel.classList.remove("error")
             dayInput.classList.remove("error")
             dayInvalidText.classList.remove("show")
             dayRequiredText.classList.remove("show")
+            invalidDateText.classList.remove("show")
+            futureDateText.classList.remove("show")
+            validDate += 1
         } else {
             console.log('invalid day')
             dayLabel.classList.add("error")
             dayInput.classList.add("error")
             dayInvalidText.classList.add("show")
             dayRequiredText.classList.remove("show")
+            invalidDateText.classList.remove("show")
+            futureDateText.classList.remove("show")
             console.log("not valid")
         }
     }
@@ -56,11 +65,22 @@ form.addEventListener("submit", (e) => {
         monthInput.classList.add("error")
     } else {
         if (monthValue > 0 && monthValue <= 12) {
+            if (monthValue == 2 && dayValue > 29) {
+                dayLabel.classList.add("error")
+                dayInput.classList.add("error")
+                monthLabel.classList.add("error")
+                monthInput.classList.add("error")
+                invalidDateText.classList.add("show")
+                monthInvalidText.classList.remove("show")
+                monthRequiredText.classList.remove("show")
+            } else {
                 console.log('valid month')
                 monthLabel.classList.remove("error")
                 monthInput.classList.remove("error")
                 monthInvalidText.classList.remove("show")
                 monthRequiredText.classList.remove("show")
+                validDate += 1
+            }
             } else {
                 console.log('invalid month')
                 monthLabel.classList.add("error")
@@ -74,12 +94,13 @@ form.addEventListener("submit", (e) => {
         yearLabel.classList.add("error")
         yearInput.classList.add("error")
     } else {
-        if (yearValue < new Date().getFullYear()) {
+        if (yearValue <= new Date().getFullYear()) {
                 console.log('valid year')
                 yearLabel.classList.remove("error")
                 yearInput.classList.remove("error")
                 yearInvalidText.classList.remove("show")
                 yearRequiredText.classList.remove("show")
+                validDate += 1
             } else {
                 console.log('invalid year')
                 yearLabel.classList.add("error")
@@ -88,7 +109,32 @@ form.addEventListener("submit", (e) => {
                 yearRequiredText.classList.remove("show")
             }
     }
+    if (validDate == 3) {
+        console.log("Valid Date")
+        let userDate = new Date(yearValue,monthValue-1,dayValue)
+        console.log(userDate)
+        let dateDiff = Date.parse(new Date()) - Date.parse(userDate)
+        if (dateDiff < 0) {
+            futureDateText.classList.add("show")
+            dayLabel.classList.add("error")
+            monthLabel.classList.add("error")
+            yearLabel.classList.add("error")
+            dayInput.classList.add("error")
+            monthInput.classList.add("error")
+            yearInput.classList.add("error")
+        } else {
+            futureDateText.classList.remove("show")
+            dayLabel.classList.remove("error")
+            monthLabel.classList.remove("error")
+            yearLabel.classList.remove("error")
+            dayInput.classList.remove("error")
+            monthInput.classList.remove("error")
+            yearInput.classList.remove("error")
+        }
+        console.log("Difference: "+dateDiff)
+    }
     console.log("submited")
+    console.log(validDate)
     // console.log(new Date().getFullYear())
     console.log("day:"+dayValue+" month:"+monthValue+" year:"+yearValue)
 })
